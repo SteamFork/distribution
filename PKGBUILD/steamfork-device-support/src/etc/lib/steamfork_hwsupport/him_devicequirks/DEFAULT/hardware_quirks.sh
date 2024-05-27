581b6f795ce67dfd2c918c9f56e45a811bfaacc7
fewtarius <fewtarius@steamfork.org>
2024-05-27 11:03:12 +0000

    * Update dirty centisecs to 6000. * Image deployment working directory bug fix. * PCIe ASPM should be enabled on AMD64 devices for best power   management.

--- PKGBUILD/steamfork-device-support/src/etc/lib/steamfork_hwsupport/him_devicequirks/DEFAULT/hardware_quirks.sh
+++ PKGBUILD/steamfork-device-support/src/etc/lib/steamfork_hwsupport/him_devicequirks/DEFAULT/hardware_quirks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export STEAMFORK_GRUB_ADDITIONAL_CMDLINEOPTIONS="radeon.modeset=1 copytoram=n nvme_load=yes amd_pstate=active amd_pstate.shared_mem=1 amdgpu.dpm=1 amdgpu.gpu_recovery=1 iomem=relaxed amdgpu.gttsize=8128 spi_amd.speed_dev=1"
+export STEAMFORK_GRUB_ADDITIONAL_CMDLINEOPTIONS="radeon.modeset=1 copytoram=n nvme_load=yes amd_pstate=active initcall_blacklist=acpi_cpufreq_init amd_pstate.shared_mem=1 amdgpu.dpm=1 amdgpu.gpu_recovery=1 iomem=relaxed amdgpu.gttsize=8128 spi_amd.speed_dev=1 pcie_aspm=force"
