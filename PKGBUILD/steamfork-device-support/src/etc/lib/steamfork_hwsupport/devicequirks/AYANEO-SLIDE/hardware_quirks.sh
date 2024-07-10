02a60b8cf207a4cb367107a6195dc7cbf06b4623
fewtarius <fewtarius@steamfork.org>
2024-07-10 20:44:05 +0000

    Kernel work, initial AYANEO Slide support.

---
+++ PKGBUILD/steamfork-device-support/src/etc/lib/steamfork_hwsupport/devicequirks/AYANEO-SLIDE/hardware_quirks.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export STEAMFORK_GAMESCOPE_ADDITIONAL_OPTIONS="--force-orientation upsidedown"
+export STEAMFORK_GAMESCOPE_RES=" -w 1280 -h 720"
+export STEAMFORK_X11_ROTATION="inverted"
