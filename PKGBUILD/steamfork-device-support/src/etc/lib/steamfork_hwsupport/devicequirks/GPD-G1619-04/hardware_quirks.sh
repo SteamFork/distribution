99fe1e37ce1c7d5fb4062f0185c1bc202cafd9cb
fewtarius <fewtarius@steamfork.org>
2024-07-14 14:10:20 +0000

    Fix up GPD Win 2 quirk, thanks to @farfenkugell!

--- PKGBUILD/steamfork-device-support/src/etc/lib/steamfork_hwsupport/devicequirks/GPD-G1619-04/hardware_quirks.sh
+++ PKGBUILD/steamfork-device-support/src/etc/lib/steamfork_hwsupport/devicequirks/GPD-G1619-04/hardware_quirks.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-export GAMESCOPE_ADDITIONAL_OPTIONS="--force-orientation right"
-export GAMESCOPE_RES="-w 1280 -h 720"
-export X11_ROTATION="right"
+export GAMESCOPE_RES="-w 1280 -h 800"
