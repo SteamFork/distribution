bb6fd7057188f878b0fc0ed0e5c75c978e8a43d8
honjow <honjow311@gmail.com>
2024-10-16 11:13:15 +0800

    Fix display screen

--- PKGBUILD/steamfork-device-support/src/etc/lib/steamfork_hwsupport/devicequirks/AYANEO-FLIP-DS/hardware_quirks.sh
+++ PKGBUILD/steamfork-device-support/src/etc/lib/steamfork_hwsupport/devicequirks/AYANEO-FLIP-DS/hardware_quirks.sh
@@ -2,4 +2,4 @@
 export X11_ROTATION=left
 export GAMESCOPE_RES="-w 1280 -h 720"
 export GAMESCOPE_ADDITIONAL_OPTIONS="--force-orientation left"
-export GAMESCOPE_DISPLAY="eDP"
+export GAMESCOPE_DISPLAY="eDP-1,eDP-2"
