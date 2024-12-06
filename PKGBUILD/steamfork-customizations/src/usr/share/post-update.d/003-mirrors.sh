c99adbdac0d650585f12bf5a8c58aa3952909413
fewtarius <fewtarius@steamfork.org>
2024-12-06 11:37:07 +0000

    Relabel SteamOS version variable.

--- PKGBUILD/steamfork-customizations/src/usr/share/post-update.d/003-mirrors.sh
+++ PKGBUILD/steamfork-customizations/src/usr/share/post-update.d/003-mirrors.sh
@@ -2,8 +2,10 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2024-present Fewtarius
 
+source /etc/os-release
+
 rm -f /etc/pacman.d/steamfork-mirrorlist
 steamfork-get-mirror random | while read line
 do
-  echo "Server = https://${line}/repos/rel" >>/etc/pacman.d/steamfork-mirrorlist
+  echo "Server = https://${line}/repos/${STEAMOS_VERSION}" >>/etc/pacman.d/steamfork-mirrorlist
 done
