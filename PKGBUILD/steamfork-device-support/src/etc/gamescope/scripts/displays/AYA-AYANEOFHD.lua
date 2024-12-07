e7cd7f44ad9b2ae51b42b86639a978951cd8d5ad
ヱッジ <uejjik@gmail.com>
2024-12-07 12:20:39 -0600

    Overhaul display scripts

--- PKGBUILD/steamfork-device-support/src/etc/gamescope/scripts/displays/AYA-AYANEOFHD.lua
+++ PKGBUILD/steamfork-device-support/src/etc/gamescope/scripts/displays/AYA-AYANEOFHD.lua
@@ -7,65 +7,48 @@ local panel_models = {
   { vendor = "AYA", model = "AYANEOFHD" },
 }
 
-local panel_colorimetry = {
-  r = { x = 0.6503, y = 0.3388 },
-  g = { x = 0.3242, y = 0.6132 },
-  b = { x = 0.1572, y = 0.0488 },
-  w = { x = 0.3134, y = 0.3291 }
-}
-
 local panel_resolutions = {
-  { width = 1080, height = 1920,
-    hfront = 90, hsync = 18, hback = 72,
-    vfront = 14,  vsync = 2, vback = 8 },
+  { width = 1080, height = 1920 },
 }
 
 local panel_refresh_rates = { 60, 120 }
 
-local panel_hdr = {
-  supported = false,
-  force_enabled = false,
-  eotf = gamescope.eotf.gamma22,
-  max_content_light_level = 500,
-  max_frame_average_luminance = 500,
-  min_content_light_level = 0.5
-}
-
 
 gamescope.config.known_displays[panel_id] = {
   pretty_name = panel_name,
-  dynamic_refresh_rates = panel_refresh_rates,
-  hdr = panel_hdr,
-  colorimetry = panel_colorimetry,
+  colorimetry = (panel_colorimetry ~= nil) and panel_colorimetry,
+  dynamic_refresh_rates = (panel_refresh_rates ~= nil) and panel_refresh_rates,
+  hdr = (panel_hdr ~= nil) and panel_hdr,
 
   dynamic_modegen = function(base_mode, refresh)
     local mode = base_mode
-    local found_res = false
-    local set_res = panel_resolutions[1]
-    debug("["..panel_id.."] Generating mode "..mode.hdisplay.."x"..mode.vdisplay.."@"..refresh.."Hz")
+    local set_res = false
+    debug("["..panel_id.."] Switching mode to "..mode.hdisplay.."x"..mode.vdisplay.."@"..refresh.."Hz")
 
     for i, res in ipairs(panel_resolutions) do
       if res.width == mode.hdisplay and res.height == mode.vdisplay then
-        found_res = true
         set_res = res
         break
       end
     end
 
-    if not found_res then
-      debug("["..panel_id.."] Generating mode failed.  Setting default resolution of "..set_res.width.."x"..set_res.height)
-      gamescope.modegen.set_resolution(mode, set_res.width, set_res.height)
+    if not set_res then
+      debug("["..panel_id.."] Mode not found.  Aborting.")
+      return mode
     end
 
-    gamescope.modegen.set_h_timings(mode, set_res.hfront, set_res.hsync, set_res.hback)
-    gamescope.modegen.set_v_timings(mode, set_res.vfront, set_res.vsync, set_res.vback)
+    if set_res.hfp ~= nil and set_res.hsync ~= nil and set_res.hbp ~= nil then
+      gamescope.modegen.set_h_timings(mode, set_res.hfp, set_res.hsync, set_res.hbp)
+    end
+    if set_res.vfp ~= nil and set_res.vsync ~= nil and set_res.vbp ~= nil then
+      gamescope.modegen.set_v_timings(mode, set_res.vfp, set_res.vsync, set_res.vbp)
+    end
 
     mode.clock = gamescope.modegen.calc_max_clock(mode, refresh)
     mode.vrefresh = gamescope.modegen.calc_vrefresh(mode)
 
     return mode
   end,
-
   matches = function(display)
     for i, panel in ipairs(panel_models) do
       if panel.vendor == display.vendor and panel.model == display.model then
