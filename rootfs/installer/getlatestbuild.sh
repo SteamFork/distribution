47bf1c3bc23bb3138c1c75b2fd3f4e2e8d5620d2
fewtarius <fewtarius@steamfork.org>
2025-01-22 11:40:19 +0000

    Checksum bug fixes.

--- rootfs/installer/getlatestbuild.sh
+++ rootfs/installer/getlatestbuild.sh
@@ -7,5 +7,4 @@ source ${IMAGE_DIR}/buildinfo
 
 mkdir -p ${DOWNLOADPATH}
 cp ${IMAGE_DIR}/${IMAGEFILE}${preferred_imgtype} ${DOWNLOADPATH}
-cp ${IMAGE_DIR}/${IMAGEFILE}${preferred_imgtype}.sha256 ${DOWNLOADPATH}
-
+sha256sum ${IMAGE_DIR}/${IMAGEFILE}${preferred_imgtype} >${DOWNLOADPATH}/${IMAGEFILE}${preferred_imgtype}.sha256
