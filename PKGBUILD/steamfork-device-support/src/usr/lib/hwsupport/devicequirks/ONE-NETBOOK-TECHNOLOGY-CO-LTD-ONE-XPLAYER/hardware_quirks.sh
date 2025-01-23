8252e9f6006b53b5569be4d7ed1e0947bc4a7380
fewtarius <fewtarius@steamfork.org>
2025-01-23 21:17:12 +0000

    Add quirk for ONEXPLAYER Mini, thanks to Joex on Discord.

---
+++ PKGBUILD/steamfork-device-support/src/usr/lib/hwsupport/devicequirks/ONE-NETBOOK-TECHNOLOGY-CO-LTD-ONE-XPLAYER/hardware_quirks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Generated quirk for `ONE-NETBOOK-TECHNOLOGY-CO-LTD-ONE-XPLAYER`
+# Save to: `/etc/lib/hwsupport/devicequirks/ONE-NETBOOK-TECHNOLOGY-CO-LTD-ONE-XPLAYER/hardware_quirks.sh`
+# Optional: Submit to the SteamFork project for official support.
+###################################################################
+export GAMESCOPE_ADDITIONAL_OPTIONS="--force-orientation left"
+export GAMESCOPE_RES=" -w 1280 -h 800"
+export X11_ROTATION="left"
+###################################################################
