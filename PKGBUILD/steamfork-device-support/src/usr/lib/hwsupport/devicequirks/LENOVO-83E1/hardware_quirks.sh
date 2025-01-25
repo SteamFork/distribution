8c08af94571e0eb02b4ed0fb26a1f44c4fe7a364
sydarn <sydarn@proton.me>
2025-01-25 15:38:18 +0000

    lgo: add device quirk

---
+++ PKGBUILD/steamfork-device-support/src/usr/lib/hwsupport/devicequirks/LENOVO-83E1/hardware_quirks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Generated quirk for `LENOVO-83E1`
+# Save to: `/etc/lib/hwsupport/devicequirks/LENOVO-83E1/hardware_quirks.sh`
+# Optional: Submit to the SteamFork project for official support.
+###################################################################
+export GAMESCOPE_RES=" -w 1280 -h 800"
+export X11_ROTATION="left"
+###################################################################
