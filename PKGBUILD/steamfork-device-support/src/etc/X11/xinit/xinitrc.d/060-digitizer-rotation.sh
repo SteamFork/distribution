858611e5a3a99e8200c00a31f69f79efe68f7445
fewtarius <fewtarius@steamfork.org>
2025-02-08 11:22:52 +0000

    Switch ROTATE_DIGITIZER to an array, and add support for configuring multiple digitizers with differing rotation requirements (Flip DS).  Thanks to @pastaq for the idea!
    ```
    export ROTATE_DIGITIZER=("/dev/input/event4:left"
    "/dev/input/event5:right")
    ```

--- PKGBUILD/steamfork-device-support/src/etc/X11/xinit/xinitrc.d/060-digitizer-rotation.sh
+++ PKGBUILD/steamfork-device-support/src/etc/X11/xinit/xinitrc.d/060-digitizer-rotation.sh
@@ -4,32 +4,44 @@
 
 source steamfork-devicequirk-set
 
-if [ "${ROTATE_DIGITIZER}" = "true" ]
+if [ -n "${ROTATE_DIGITIZER[0]}" ]
 then
-	case ${X11_ROTATION} in
-		left)
-			X11_TOUCH="0 -1 1 1 0 0 0 0 1"
-			;;
-		right)
-			X11_TOUCH="0 1 0 -1 0 1 0 0 1"
-			;;
-		normal)
-			X11_TOUCH="1 0 0 0 1 0 0 0 1"
-			;;
-		inverted)
-			X11_TOUCH="-1 0 1 0 -1 1 0 0 1"
-			;;
-	esac
-
-	cat <<EOF | sudo tee /etc/X11/xorg.conf.d/99-touchscreen_orientation.conf
+	if [ -f "/etc/X11/xorg.conf.d/99-touchscreen_orientation.conf" ]
+	then
+		rm -f "/etc/X11/xorg.conf.d/99-touchscreen_orientation.conf"
+	fi
+	for DIGITIZER in "${ROTATE_DIGITIZER[@]}"
+	do
+		EVENT_DEVICE="${DIGITIZER%:*}"
+		CALIBRATION="${DIGITIZER#*:}"
+		case ${CALIBRATION} in
+			left)
+				X11_TOUCH="0 -1 1 1 0 0 0 0 1"
+				;;
+			right)
+				X11_TOUCH="0 1 0 -1 0 1 0 0 1"
+				;;
+			normal)
+				X11_TOUCH="1 0 0 0 1 0 0 0 1"
+				;;
+			inverted)
+				X11_TOUCH="-1 0 1 0 -1 1 0 0 1"
+				;;
+			*)
+				X11_TOUCH="${CALIBRATION}"
+				;;
+		esac
+	
+		cat <<EOF | sudo tee -a /etc/X11/xorg.conf.d/99-touchscreen_orientation.conf
 Section "InputClass"
 	Identifier "Coordinate Transformation Matrix"
 	MatchIsTouchscreen "on"
-	MatchDevicePath "/dev/input/event*"
+	MatchDevicePath "${EVENT_DEVICE}"
 	MatchDriver "libinput"
 	Option "CalibrationMatrix" "${X11_TOUCH}"
 EndSection
 EOF
+	done
 else
 	if [ -f "/etc/X11/xorg.conf.d/99-touchscreen_orientation.conf" ]
 	then
