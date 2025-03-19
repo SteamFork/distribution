164362475a6e468fae0d2744b16145895831fe7c
fewtarius <fewtarius@steamfork.org>
2025-03-19 15:52:05 +0000

    Gamescope should use plasma-wayland for desktop mode, and we should enable the OSK.

---
+++ PKGBUILD/steamfork-customizations/src/usr/share/post-update.d/006-touch-input.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+FILE_PATH="/home/deck/.config/kwinrc"
+WAYLAND_SECTION="[Wayland]"
+INPUT_METHOD="InputMethod[\$e]=/usr/share/applications/com.github.maliit.keyboard.desktop"
+
+if [ ! -f "$FILE_PATH" ]; then
+    # Create the file with the initial content
+    cat <<EOF > "$FILE_PATH"
+$WAYLAND_SECTION
+$INPUT_METHOD
+EOF
+else
+    if grep -q "^\[Wayland\]" "$FILE_PATH"; then
+        if grep -q "^InputMethod\[\$e\]=" "$FILE_PATH"; then
+            sed -i "s|^InputMethod\[\$e\]=.*|$INPUT_METHOD|" "$FILE_PATH"
+        else
+            sed -i "/^\[Wayland\]/a $INPUT_METHOD" "$FILE_PATH"
+        fi
+    else
+        echo -e "\n$WAYLAND_SECTION\n$INPUT_METHOD" >> "$FILE_PATH"
+    fi
+fi
+
+chown deck:deck ${FILE_PATH}
+chmod 0600 ${FILE_PATH}
