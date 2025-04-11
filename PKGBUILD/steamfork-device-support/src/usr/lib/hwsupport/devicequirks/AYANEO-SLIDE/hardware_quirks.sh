7f8268003609fad1de233f38f68675dcbe48ec31
ヱッジ <uejjik@gmail.com>
2025-04-11 16:01:41 -0500

    Clean up hardware quirks files
    Add GPD Pocket 4 orientation patch

--- PKGBUILD/steamfork-device-support/src/usr/lib/hwsupport/devicequirks/AYANEO-SLIDE/hardware_quirks.sh
+++ PKGBUILD/steamfork-device-support/src/usr/lib/hwsupport/devicequirks/AYANEO-SLIDE/hardware_quirks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export GAMESCOPE_ADDITIONAL_OPTIONS="--force-orientation left"
-export GAMESCOPE_RES=" -w 1280 -h 720"
+export GAMESCOPE_RES="-w 1280 -h 720"
 export X11_ROTATION="left"
