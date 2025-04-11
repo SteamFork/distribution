118cf9310184c9fbfa49f6f7e85ee3d55e04bc3b
ヱッジ <uejjik@gmail.com>
2025-04-11 16:31:47 -0500

    Clean up hardware quirks files

--- PKGBUILD/steamfork-device-support/src/usr/lib/hwsupport/devicequirks/AYA-NEO-AYA-NEO-Founder/hardware_quirks.sh
+++ PKGBUILD/steamfork-device-support/src/usr/lib/hwsupport/devicequirks/AYA-NEO-AYA-NEO-Founder/hardware_quirks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export X11_ROTATION=left
-export GAMESCOPE_RES="-w 1280 -h 800"
 export GAMESCOPE_ADDITIONAL_OPTIONS="--force-orientation right"
+export GAMESCOPE_RES="-w 1280 -h 800"
+export X11_ROTATION="left"
