42df3db00e1edd25a08c79dd58737ac1cff2670a
ヱッジ <uejjik@gmail.com>
2025-04-11 16:01:41 -0500

    Clean up hardware quirks files
    Add GPD Pocket 4 orientation patch

--- PKGBUILD/steamfork-device-support/src/usr/lib/hwsupport/devicequirks/AYANEO-AYANEO-2021-Pro-Retro-Power/hardware_quirks.sh
+++ PKGBUILD/steamfork-device-support/src/usr/lib/hwsupport/devicequirks/AYANEO-AYANEO-2021-Pro-Retro-Power/hardware_quirks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export X11_ROTATION=inverted
-export GAMESCOPE_RES="-w 1280 -h 800"
 export GAMESCOPE_ADDITIONAL_OPTIONS="--force-orientation upsidedown"
-export STEAMFORK_GRUB_ADDITIONAL_CMDLINEOPTIONS+=" module_blacklist=ayaneo_platform"
+export GAMESCOPE_RES="-w 1280 -h 800"
+export X11_ROTATION="inverted"
+export STEAMFORK_GRUB_ADDITIONAL_CMDLINEOPTIONS="${STEAMFORK_GRUB_ADDITIONAL_CMDLINEOPTIONS} module_blacklist=ayaneo_platform"
--- PKGBUILD/steamfork-device-support/src/usr/lib/hwsupport/devicequirks/ayn-Loki-MiniPro/hardware_quirks.sh
+++ PKGBUILD/steamfork-device-support/src/usr/lib/hwsupport/devicequirks/ayn-Loki-MiniPro/hardware_quirks.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Generated quirk for `ayn-Loki-MiniPro`
-# Save to: `/etc/lib/hwsupport/devicequirks/ayn-Loki-MiniPro/hardware_quirks.sh`
-# Optional: Submit to the SteamFork project for official support.
-###################################################################
 export GAMESCOPE_ADDITIONAL_OPTIONS="--force-orientation left"
-export GAMESCOPE_RES=" -w 1280 -h 720"
+export GAMESCOPE_RES="-w 1280 -h 720"
 export X11_ROTATION="left"
-###################################################################
